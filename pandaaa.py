d4a18e1e1a6aa2b596965cc41180eb0f7840b4b3
Simonjulia <73659787+Simonjulia@users.noreply.github.com>
2021-12-02 15:30:54 +0100

    Add files via upload

--- pandaaa.py
+++ pandaaa.py
@@ -9,7 +9,8 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import Point3
+from panda3d.core import Point3, KeyboardButton
+
 
 class MyApp(ShowBase):
 
@@ -24,10 +25,6 @@ class MyApp(ShowBase):
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
         
-        # Add the spinCameraTask procedure to the task manager.
-        #self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-        #self.taskMgr.add(self.spinCameraTask2, "SpinCameraTask2")
-        
         # Load and transform the panda actor.
         self.pandaActor = Actor("models/panda-model",
                                 {"walk": "models/panda-walk4"})
@@ -37,54 +34,75 @@ class MyApp(ShowBase):
         self.pandaActor.reparentTo(self.render)
         # Loop its animation.
 
-        self.accept('d', self.move_right,[1] )
-        self.accept("d-up", self.move_right, [0])
-        self.accept('q', self.move_left,[1] )
-        self.accept("q-up", self.move_left, [0])
-        self.accept('space', self.jump )
+        self.taskMgr.add(self.move_task, "move_task")
+        self.taskMgr.add(self.jump_task, "jump_task")
 
+    def jump_task(self,task) :
 
-    def move_right(self, value) :
-        self.pandaActor.setHpr(90, 0, 0)
-        global posInterval1
-        position = self.pandaActor.getPos()
- 
-        if value ==1 :
-            posInterval1 = self.pandaActor.posInterval(100,
-                                                       position + Point3(20,0,0),
-                                                       startPos=position)
-            posInterval1.start()
-            self.pandaActor.loop("walk")
-        else :
-            posInterval1.pause()
-            self.pandaActor.stop()
-
-    def move_left(self, value) :
-        self.pandaActor.setHpr(-90, 0, 0)
-        global posInterval2
-        position = self.pandaActor.getPos()
- 
-        if value ==1 :
-            posInterval2 = self.pandaActor.posInterval(100,
-                                                       position + Point3(-20,0,0),
-                                                       startPos=position)
-            posInterval2.start()
-            self.pandaActor.loop("walk")
-        else :
-            posInterval2.pause()
-            self.pandaActor.stop()
-
-    def jump(self) :
-
-        position = self.pandaActor.getPos()
-        posInterval3 = self.pandaActor.posInterval(0.5,
-                                                       position + Point3(0,0,0.4),
-                                                       startPos=position)
-        posInterval4 = self.pandaActor.posInterval(0.5,
-                                                       position,
-                                                       startPos=position + Point3(0,0,0.4))
-        Sequence(posInterval3,posInterval4).start()
         
+        z = self.pandaActor.getZ()
+        if z <= -0.3 :
+            is_down = base.mouseWatcherNode.is_button_down
+            space_button = KeyboardButton.space()
+            if is_down(space_button):
+                self.taskMgr.add(self.up,"up")
+                print('lee-sin = noob')
+        """x_delta = speed * globalClock.get_dt()
+        position = self.pandaActor.getPos()"""
+    
+        return Task.cont
+
+    def up(self,task) :
+        speed_jumpu = 2.5
+        z_delta = speed_jumpu * globalClock.get_dt()
+        z = self.pandaActor.getZ()
+        self.pandaActor.setZ(z + z_delta)
+
+        if z + z_delta > 0.3 :
+            self.taskMgr.add(self.down,"down")
+            return Task.done
+        else :
+            return Task.cont
+
+    def down(self,task) :
+        speed_jumpd = 1.5
+        z_deltad = speed_jumpd * globalClock.get_dt()
+        zd = self.pandaActor.getZ()
+        self.pandaActor.setZ(zd - z_deltad)
+
+        if zd + z_deltad <= -0.3 :
+            self.pandaActor.setZ(-0.3)
+            return Task.done
+        else :
+            return Task.cont
+        
+    def move_task(self, task):
+        speed = 0
+        right_speed = 1 # units per second
+        left_speed = 1
+        right_button = KeyboardButton.ascii_key('d')
+        left_button = KeyboardButton.ascii_key('q')
+        # Check if the player is holding W or S
+        
+        is_down = base.mouseWatcherNode.is_button_down
+
+        if is_down(right_button):
+            speed += right_speed
+
+        if is_down(left_button):
+            speed -= left_speed
+
+        if speed > 0 :
+            self.pandaActor.setHpr(90, 0, 0)
+        elif speed < 0 :
+            self.pandaActor.setHpr(-90, 0, 0)
+        # Move the player
+        x_delta = speed * globalClock.get_dt()
+        position = self.pandaActor.getPos()
+
+        self.pandaActor.setPos(position + Point3(x_delta,0,0))
+        return Task.cont
+
 
 app = MyApp()
 app.run()
